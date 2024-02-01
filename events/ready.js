f8b066903c2875f28ced4f95bf9a0381042c11ca
Zyad M. Ayad <zyad.moh.1011@gmail.com>
2024-02-01 21:21:39 +0200

    add status to bot

--- events/ready.js
+++ events/ready.js
@@ -5,5 +5,20 @@ module.exports = {
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+
+		const botStatusList = [
+			"/help | Watching your messages",
+			"/help | Did you delete something?",
+			"/help | I've seen that edit",
+			"/help | I'm watching you",
+			"/help | you are on the list",
+		];
+
+		setInterval(() => {
+			const index = Math.floor(Math.random() * (botStatusList.length - 1) + 1);
+			client.user.setActivity(botStatusList[index]);
+		}, 10000);
+
+
 	},
 };  
