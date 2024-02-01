4c9ee1eeb8bfa48c0dbb6af68f42753c5e5d5f15
Zyad M. Ayad <zyad.moh.1011@gmail.com>
2024-02-01 14:50:39 +0200

    fix help command

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -11,28 +11,31 @@ module.exports = {
             .setDescription('Chat observer is a unique bot that observe your discord server messages.')
             .addFields(
                 {
-                    name : "Required permissions",
-                    value : `The bot itself just need to be able to read messages in the channels you want to observe, and send messages in the log channel.
+                    name: "Required permissions",
+                    value: `The bot itself just need to be able to read messages in the channels you want to observe, and send messages in the log channel.
                     But for you to be able to manage the bot, you need to have the following permissions:
                     \`Manage Messages\`, \`Manage Channels\`, \`Manage Server\``,
-                    inline : false
+                    inline: false
                 },
                 {
-                    name : "Log Channel",
-                    value : `The log channel is the channel the bot will send the logs to.
+                    name: "Log Channel",
+                    value: `The log channel is the channel the bot will send the logs to.
                     To set the log channel, use \`/logchannel\` command.`
                 },
                 {
-                    name : "List",
-                    value : `The list is the list of channels the bot will observe.
+                    name: "List",
+                    value: `The list is the list of channels the bot will observe.
                     To add or remove channels from the list, use \`/list\` command.
                     To swap between black list and white list, use \`/listtype\` command.
                     By default, the bot will observe all channels it has read permissions in. i.e the list is empty and the list type is black list.`
                 },
             )
             .setColor('#0000ff')
-            .setFooter('Chat Observer')
-            .addTimestamp();
+            .setFooter({
+                text: "Chat Observer",
+                iconURL: client.user.avatarURL(),
+            })
+            .setTimestamp();
 
         await interaction.reply({ embeds: [embed], ephemeral: true });
 
